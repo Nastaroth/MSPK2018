83fe59287bcb5353ddbf94219881121f3aa72335
nastaroth <antonioradikal@gmail.com>
2018-12-26 22:58:59 -0400

    Pagina2 MSPK2018

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require ('path');
 
 //setting
-app.set('port',3000)
+app.set('port',8080)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
