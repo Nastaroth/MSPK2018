56d4bf19e0eadaac4fa2184e4f8066f0698ab60b
nastaroth <antonioradikal@gmail.com>
2018-12-27 20:19:57 -0400

    Modificacion del puerto

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const path = require ('path');
+var port = process.env.PORT || 3000;
 
 //setting
-app.set('port',8080)
+//app.set('port',3000)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -20,6 +21,12 @@ app.use(require('./routes/index'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 //listening the server
-app.listen(app.get('port'), () =>{
+/*app.listen(app.get('port'), () =>{
     console.log('server works', app.get('port'))
+});*/
+
+
+
+app.listen(port, "0.0.0.0", function() {
+console.log("Listening on Port 3000");
 });
